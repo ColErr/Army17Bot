80d0251ae4da79aab92ebaf38cf1dc9a1c1a0273
Andrew Canino <jkripper@gmail.com>
2018-12-08 21:25:33 -0800

    Added comments and a graceful shutdown

--- src/main.py
+++ src/main.py
@@ -3,16 +3,28 @@ import asyncio
 
 client = discord.Client()
 
+# On connection, output bot name to console
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
+    
+    # Change "Playing" text
+    await client.change_presence(game=discord.Game(name='with myself'))
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
+    # Exits the bot iff I send the shutdown command
+    if message.content.startswith('^quit') and message.author.id == '92305780048420864':
+        tmp = await client.send_message(message.channel, "I am quitting")
+        print("I am quitting because of channel command")
+        client.logout()
+        quit()
+    #Demo code from discord.py retained for now as reference
+    '''
+    elif message.content.startswith('^test'):
         counter = 0
         tmp = await client.send_message(message.channel, 'Calculating messages...')
         async for log in client.logs_from(message.channel, limit=100):
@@ -20,8 +32,8 @@ async def on_message(message):
                 counter += 1
 
         await client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!sleep'):
+    elif message.content.startswith('^sleep'):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
-
+    '''
 client.run('!!!TOKEN!!!')
